b0449345beaa3afa811291c3606495ccae718ee1
lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2017-12-05 14:50:55 +0100

    updating stuff

--- python/pack-code.sh
+++ python/pack-code.sh
@@ -3,4 +3,4 @@ if [ -f code.zip ];
 then
     rm code.zip
 fi
-zip code.zip eval_score_logs.py gait_experiments.py training.py
+zip code.zip eval_score_logs.py gait_experiments.py training.py perturbations.py perturbation_experiments.py perturb_and_pack.sh
