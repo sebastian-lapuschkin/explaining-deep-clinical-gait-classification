41d748938eb967077e35ced13435f18a0e14c46d
Sebastian Roland Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2019-01-14 18:40:03 +0100

    remobing obsolte data and convenience scripts

--- python/pack-code.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -f code.zip ];
-then
-    rm code.zip
-fi
-zip code.zip eval_score_logs.py gait_experiments.py training.py perturbations.py perturbation_experiments.py perturb_and_pack.sh
--- python/restart_training_lotsatimes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while true ; do
-    python gait_experiments.py skip=0 $@
-done
