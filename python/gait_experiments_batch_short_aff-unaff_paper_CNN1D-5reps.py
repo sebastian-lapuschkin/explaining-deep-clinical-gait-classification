fa25f563f8b6223b372b731e3adc82a217d04a0a
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2020-06-26 13:35:47 +0200

    adding some jobs for djordje

---
+++ python/gait_experiments_batch_short_aff-unaff_paper_CNN1D-5reps.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-	
+"""
+Created on Thu Oct  3 08:26:07 2019
+
+@author: horst
+"""	
+	
+# 1-234	
+runfile('V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python\gait_experiments.py', args='-d ./data/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -o ./output-rep-1/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -a Cnn1DC8', wdir='V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python')	
+
+
+# 1-234	
+runfile('V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python\gait_experiments.py', args='-d ./data/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -o ./output-rep-2/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -a Cnn1DC8', wdir='V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python')	
+
+
+# 1-234	
+runfile('V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python\gait_experiments.py', args='-d ./data/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -o ./output-rep-3/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -a Cnn1DC8', wdir='V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python')	
+
+
+# 1-234	
+runfile('V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python\gait_experiments.py', args='-d ./data/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -o ./output-rep-4/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -a Cnn1DC8', wdir='V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python')	
+
+
+# 1-234	
+runfile('V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python\gait_experiments.py', args='-d ./data/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -o ./output-rep-5/2019_frontiers_small_dataset_v3_aff-unaff-atMM_1-234_ -a Cnn1DC8', wdir='V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\interpretable-deep-gait-injury\python')	
+
