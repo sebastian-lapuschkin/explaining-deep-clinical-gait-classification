e36b74594d6b71248cefe1ef12b73ebdfd5554f2
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2021-06-02 16:25:53 +0200

    comments

--- python/install_metaanalysis.sh
+++ python/install_metaanalysis.sh
@@ -2,7 +2,7 @@
 
 # install corelay + dependecies for meta analyses based on xai
 pip install 'git+https://github.com/virelay/corelay'
-#pip install 'git+git://github.com/virelay/corelay#egg=corelay[umap,hdbscan]'
+#pip install 'git+git://github.com/virelay/corelay#egg=corelay[umap,hdbscan]' # importing umap makes everything slow. we do not need it.
 pip install h5py
 
 
