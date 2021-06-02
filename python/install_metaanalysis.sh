c00445f4029fdb29773bfa454ad8dd526c237dd1
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2021-06-02 10:22:31 +0200

    streamlining requirements and installation

--- python/install_metaanalysis.sh
+++ python/install_metaanalysis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install corelay + dependecies for meta analyses based on xai
-pip install 'git+git://github.com/virelay/corelay'
-pip install 'git+git://github.com/virelay/corelay#egg=corelay[umap,hdbscan]'
+pip install 'git+https://github.com/virelay/corelay'
+#pip install 'git+git://github.com/virelay/corelay#egg=corelay[umap,hdbscan]'
 
 
 
