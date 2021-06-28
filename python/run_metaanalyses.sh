9617543eb050876cd1c14011d0c02579e84d667f
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2021-06-28 10:52:09 +0200

    adapting run script for metaanalyses

--- python/run_metaanalyses.sh
+++ python/run_metaanalyses.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-for tp in 12 ; do # pair tp 12 with gi 0, tp 30 with gi 1 , ie scales the visualized embedding wrt number of involved datapoints per class
-for gi in 0 ; do
+
 for model in Cnn1DC8 ; do
 for fold in {0..9} ; do
-    python main_metaanalysis.py -tp $tp -cmaps tab20 -f $fold -gi $gi -m $model -sr # -s # no showing
+    python main_metaanalysis.py -cmaps tab20 -f $fold -gi 0 -tp 12 -m $model -sr # -s # no showing
+    python main_metaanalysis.py -cmaps tab20 -f $fold -gi 1 -tp 30 -m $model -sr # -s # no showing
 done
 done
-done
-done
+
