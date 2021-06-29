0008c91d00459c01709107feceebd27d7c4dd395
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2021-06-29 12:03:27 +0200

    corelay can now be installed via pip

--- python/install_metaanalysis.sh
+++ python/install_metaanalysis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install corelay + dependecies for meta analyses based on xai
-pip install 'git+https://github.com/virelay/corelay'
-#pip install 'git+git://github.com/virelay/corelay#egg=corelay[umap,hdbscan]' # importing umap makes everything slow. we do not need it.
+pip install corelay
+#pip install corelay[umap,hdbscan] # importing umap makes everything slow. we do not need it.
 pip install h5py
 pip install termcolor
 
