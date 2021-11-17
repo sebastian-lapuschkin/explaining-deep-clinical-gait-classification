0bc103c87ed646fae79967bd4a6ea5041b2a4651
fabian-horst <horst@uni-mainz.de>
2021-11-17 15:04:27 +0100

    updated data and batch files
    updated data and batch files

---
+++ python/gait_experiments_batch_short_aff-unaff_paper_atMM-0-1_vel_matched.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-	
+"""
+Created on Thu Oct  3 08:26:07 2019
+
+@author: horst
+"""	
+	
+# 1-3	
+runfile('V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\explaining-deep-gait-classification-master\python\gait_experiments.py', args='-d ./data/2021_acm_small_dataset_v3_subset_velocity_matched_aff-unaff-atMM_1-3_.mat -o ./output/2021_acm_small_dataset_v3_subset_velocity_matched_aff-unaff-atMM_1-3_ -a Cnn1DC8', wdir='V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\explaining-deep-gait-classification-master\python')	
+
+runfile('V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\explaining-deep-gait-classification-master\python\gait_experiments.py', args='-d ./data/2021_acm_small_dataset_v3_subset_velocity_matched_aff-unaff-nonorm_1-3_.mat -o ./output/2021_acm_small_dataset_v3_subset_velocity_matched_aff-unaff-nonorm_1-3_ -a Cnn1DC8', wdir='V:\Trainingswissenschaft\Horst\Gait - XAI - Clinical\explaining-deep-gait-classification-master\python')	
